a66ba4617d667629e2dc3bcdc15af9f54b54fc08
Arthur Vinicius De Sena Moraes <123411782+lngg21@users.noreply.github.com>
2024-05-29 12:04:55 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HEADER = 64
 PORT = 5050
@@ -7,25 +8,33 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 # Whatever IP address you found from running ifconfig in terminal.
 # SERVER = ""
 SERVER = socket.gethostbyname(socket.gethostname())
-
 ADDR = (SERVER, PORT)
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Officially connecting to the server.
 client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+def send_msg():
+    while True:
+        msg = input()
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(message)
+        if msg == DISCONNECT_MESSAGE:
+                break
+        
+def receive_msg():
+        while True:
+                msg_length = client.recv(HEADER).decode(FORMAT)
+                if msg_length:
+                    msg_length = int(msg_length)
+                    msg = client.recv(msg_length).decode(FORMAT)
+                    print(msg)
 
-send("Hello World")
-input()
-send("Hello Matt")
-input()
-send("Hello Everyone")
-input()
-send(DISCONNECT_MESSAGE)
+thread_send = threading.Thread(target=send_msg)
+thread_receive = threading.Thread(target=receive_msg)
+thread_send.start()
+thread_receive.start()
